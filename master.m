b925052ed2608b4de19fa32817de152c4567cc4c
KYLE ANDREW KURKELA <kak53@comp-ic-0006.aci.ics.psu.edu>
2017-05-28 15:28:15 -0400

    Updated master script
    1.) Can skip over section if desired
    2.) Will email updates and email on errors

--- master.m
+++ master.m
@@ -3,3 +3,70 @@
 % Written by Kyle Kurkela, kyleakurkela@gmail.com
 % May-June, 2017
 
+runSpec = true;
+runEst  = true;
+runST   = true;
+runRSA  = true;
+
+%% Step 1: Specify the Model
+
+if runSpec
+
+sendmail('kyleakurkela@gmail.com', 'Specifying...')
+
+pause off
+
+try
+    SpecifyModel;
+catch
+    sendmail('kyleakurkela@gmail.com', 'Problem with Specifying')
+end
+
+pause on
+
+end
+
+%% Step 2: Estimate Model
+
+if runEst
+
+sendmail('kyleakurkela@gmail.com', 'Estimating...')
+
+try
+    EstimateModel;
+catch
+    sendmail('kyleakurkela@gmail.com', 'Problem with Estimating')
+end
+
+end
+
+%% Step 3: Generate Single Trial Models
+
+if runST
+
+sendmail('kyleakurkela@gmail.com', 'Single Trial...')
+
+try
+    generate_spm_singletrial;
+catch
+    sendmail('kyleakurkela@gmail.com', 'Problem Generating Single Trial')
+end
+
+end
+
+%% Step 4: run RSA
+
+if runRSA
+
+% copy over the ROIs
+copyfile('/gpfs/group/nad12/default/nad12/FAME8/Analysis_ret/FAMEret8RSA_hrf/*.nii', '/gpfs/group/nad12/default/nad12/FAME8/RSA/models/SingleTrialModel/')
+
+sendmail('kyleakurkela@gmail.com', 'RSA...')
+
+try
+    run_CANLAB_rsa;
+catch
+    sendmail('kyleakurkela@gmail.com', 'Problem with RSA')
+end
+
+end
